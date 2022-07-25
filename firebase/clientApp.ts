199e86462e9ebb4adf3937450f5efab9ed066ea1
Brayan Muniz <brayanMuniz2636@gmail.com>
2022-07-25 18:56:24 -0500

    Firebase Config

---
+++ firebase/clientApp.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCO1IjLNVwZ96TNq7qDMfm-R2hrkvzzqXg",
+  authDomain: "power-d44d6.firebaseapp.com",
+  projectId: "power-d44d6",
+  storageBucket: "power-d44d6.appspot.com",
+  messagingSenderId: "897500147704",
+  appId: "1:897500147704:web:529a58b03ea4ef4698f00d",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
